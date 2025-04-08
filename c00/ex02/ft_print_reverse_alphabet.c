ea16426d3ef82fa8ae32b5ce1312ce1a52a87652
ciaacov <icoiaacov@gmail.com>
2025-04-08 15:33:44 +0100

    C00

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ciaacov- <ciaacov-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 21:07:30 by ciaacov-          #+#    #+#             */
+/*   Updated: 2025/02/23 17:17:02 by ciaacov-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write (1, &c, 1);
+		c--;
+	}
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+}
---
+++ c00/ex04/ft_is_negative.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ciaacov- <ciaacov-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 21:43:23 by ciaacov-          #+#    #+#             */
+/*   Updated: 2025/02/23 17:25:20 by ciaacov-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	if (n >= 0)
+	{
+		n = 'P';
+	}
+	else
+	{
+		n = 'N';
+	}
+	write(1, &n, 1);
+}
+
+int	main(void)
+{
+	ft_is_negative(-9);
+	ft_is_negative(9);
+	ft_is_negative(8);
+	ft_is_negative(-5);
+	return (0);
+}
